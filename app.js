5951c9dbcf42c54569f60f79c7493f12cef3e2d1
Tesh Patel <tesh.p@hotmail.co.uk>
2021-10-18 21:44:13 +0100

    Add .

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
 const mainArea = document.getElementById("game-container");
-const newDiv = document.createElement("div");
+//const newDiv = document.createElement("div");
 const gridInput = document.querySelectorAll("input");
 const output = document.getElementById("output");
 const gameContainer = document.getElementById("game-container")
 const resetBtn = document.getElementById("reset-btn");
 const divsToRemove = document.getElementsByClassName("newDivs");
-let colorPicker = document.getElementById("colorpicker").value;
+// let colorPicker = document.getElementById("colorpicker").value;
 
+// Slider bar update and generate grids on board
 // Restart game/clean board 
 function resetGrid() {
     for (let i = divsToRemove.length - 1;i >= 0;i--) {
         divsToRemove[i].remove();
     }
 };
-
-resetBtn.addEventListener("click", resetGrid);
-
-
-// Slider bar update and generate grids on board
-function displayNoOfGrids() {
-    document.getElementById("output").innerText = gridNo.value;
-    generateGameBoard()
-};
-
-gridInput.forEach(input => input.addEventListener("change", displayNoOfGrids));
 
 // Generate game grids
 function generateGameBoard() {
@@ -36,34 +26,46 @@ function generateGameBoard() {
         const newDiv = document.createElement("div");
         newDiv.classList.add("newDivs");
         document.getElementById("game-container").appendChild(newDiv);
+        newDiv.addEventListener("mouseover", brushMode);
     }
+
+};
+
+function displayNoOfGrids() {
+    document.getElementById("output").innerText = gridNo.value;
+    generateGameBoard()
+};
+
+
+// return random color for psychedlic feature
+function randomColor() {
+    const randomColorVal = Math.floor(Math.random() * 16777215).toString(16);
+    return randColor = "#" + randomColorVal;
+
 }
 
-// // generate random color
-// function randomColor() {
-//     const randomColorVal = Math.floor(Math.random() * 16777215).toString(16);
-//     return randColor = "#" + randomColorVal;
-// }
-
-// function brushMode() {
-// select grid
-
-// onhover - change the selected grids color to value of colur picker
-
-//     newDiv.style.backgroundColor = `{colorPicker}`;
-// }
-
-// newDiv.addEventListener("mouseover", brushMode())
 
 
+// Brush mode - change color of div
+function brushMode() {
+    let colorPicker = document.getElementById("colorpicker").value;
+    event.target.style.backgroundColor = `${colorPicker}`;
+};
 
-// create function - for single color // make color picker function // change color of each div to value of color picker
-// create function - random color - psychedlic mode // change color of each div to randomColor return value
-// create function - eraser
+// Psychedelic mode
+function psychMode() {
+    let colorPicker = document.getElementById("colorpicker").value;
+    event.target.style.backgroundColor = randomColor();
+};
 
-// Create functions first then add event listeners to on:hover on divs
+// event listeners
+gridInput.forEach(input => input.addEventListener("change", displayNoOfGrids));
+resetBtn.addEventListener("click", resetGrid);
 
-// design CSS at the end
-// feedback from TOP
-// display the number from range bar - DONE
-// create function to reset/reload game - DONE
+
+
+
+// -- TO DO --
+// Single Color Mode
+// Psychedlic Mode
+// Eraser feature
