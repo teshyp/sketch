bba45c842024000b4ccb73ed3220c3e2022ccc2b
Tesh Patel <tesh.p@hotmail.co.uk>
2021-10-20 21:12:34 +0100

    Working on Brush mode and multicolor mode features

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const output = document.getElementById("output");
 const gameContainer = document.getElementById("game-container")
 const resetBtn = document.getElementById("reset-btn");
 const divsToRemove = document.getElementsByClassName("newDivs");
-// const eraserBtn = document.getElementById("eraser-btn");
+const eraserBtn = document.getElementById("eraser-btn");
 const psyBtn = document.getElementById("psy-btn");
-// const brushBtn = document.getElementById("clr-btn");
-// let colorPicker = document.getElementById("colorpicker").value;
+const brushBtn = document.getElementById("clr-btn");
+//let colorPicker = document.getElementById("colorpicker").value;
 
-// Slider bar update and generate grids on board
-// Restart game/clean board 
+// Restart game/clean board - Brush mode
 function resetGrid() {
     for (let i = divsToRemove.length - 1;i >= 0;i--) {
         divsToRemove[i].remove();
@@ -22,58 +21,58 @@ function resetGrid() {
 function generateGameBoard() {
 
     resetGrid()
+    document.getElementById("output").innerText = gridNo.value;
 
     for (let i = 0;i < parseInt(output.value * output.value);i++) {
+
         gameContainer.style.gridTemplateColumns = `repeat(${output.value}, 1fr)`
         gameContainer.style.gridTemplateRows = `repeat(${output.value}, 1fr)`
         const newDiv = document.createElement("div");
         newDiv.classList.add("newDivs");
         document.getElementById("game-container").appendChild(newDiv);
-
-        // ADD function or IF - if brush is clicked then burshMode is Psy is clicked Psych mode.
-        newDiv.addEventListener("mouseover", psychMode);
-        //newDiv.addEventListener("mouseover", brushMode);
-
+        newDiv.addEventListener("mouseover", clrPickermMode);
+        //newDiv.addEventListener("mouseover", psychMode);
     }
-
-};
-
-function displayNoOfGrids() {
-    document.getElementById("output").innerText = gridNo.value;
-    generateGameBoard()
 };
 
 
-// return random color for psychedlic feature
-function randomColor() {
-    const randomColorVal = Math.floor(Math.random() * 16777215).toString(16);
-    return randColor = "#" + randomColorVal;
-
-}
-
+// Eraser mode 
+function eraser() {
+    event.target.style.backgroundColor = "#FFFFFF";
+};
 
 
 // Brush mode - change color of div
-function brushMode() {
+function clrPickermMode() {
     let colorPicker = document.getElementById("colorpicker").value;
+    eraserBtn.addEventListener("mouseover", eraser);
     event.target.style.backgroundColor = `${colorPicker}`;
 };
 
+
 // Psychedelic mode
 function psychMode() {
-    let colorPicker = document.getElementById("colorpicker").value;
-    event.target.style.backgroundColor = randomColor();
+    document.getElementsByClassName("newDivs").addEventListener("mouseover", psyDiv);
+    function psyDiv() {
+        randomColorVal = Math.floor(Math.random() * 16777215).toString(16);
+        event.target.style.backgroundColor = `${"#" + randomColorVal}`;
+    }
 };
 
+
 // event listeners
-gridInput.forEach(input => input.addEventListener("change", displayNoOfGrids));
+gridInput.forEach(input => input.addEventListener("change", generateGameBoard));
+
+
 resetBtn.addEventListener("click", resetGrid);
-psyBtn.addEventListener("click", psychMode);
-
-
 
 
 // -- TO DO --
 // Single Color Mode
 // Psychedlic Mode
 // Eraser feature
+
+// Load page
+// reset grid 
+// Default mode - paint with color picker
+// 
